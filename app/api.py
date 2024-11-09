4f08adc2ca10b070daa25c62c0386bea84f9b6ff
Kassim <kassimorra@gmail.com>
2024-11-09 18:05:29 -0300

    remover api.py

--- app/api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from api_help.get_data import get_data
-
-app = FastAPI()
-
-@app.get("/yfinance/")
-def yfinance():
-    return JSONResponse(content=get_data())
