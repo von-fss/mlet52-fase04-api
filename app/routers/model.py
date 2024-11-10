e3dd5f9f7fd79eccf7e652a974d79cb8e91acaa6
Kassim <kassimorra@gmail.com>
2024-11-10 20:38:17 -0300

    APi adequada para retornar a lista de Tickers, treinar e usar o modelo

--- app/routers/model.py
+++ app/routers/model.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
-import torch
-import torch.nn as nn
+from app.internal.model import train, predict
 
 router = APIRouter(
     prefix="/models",
@@ -9,10 +7,28 @@ router = APIRouter(
     responses={404: {'description': 'Not Found'}}
 )
 
-@router.get('/train')
-def train_model():
-    return {"ok": 'ok'}
 
-@router.post('/')
-def value_model():
-    return {{"ok": 'ok'}}
+@router.get('/train')
+def train_model(ticker: str):
+    """
+    Description:
+        Recebe o código de um ticker, treina o modelo e salva na pasta train_model
+
+    Args:
+        ticker: str -> Código do ticker a ser analisado
+    """
+    train(ticker)
+    return {"result": 'modelo treinado com sucesso'}
+
+
+@router.post('/predict')
+def value_model(ticker: str):
+    """
+    Description:
+        Procura por um modelo já gerado e fazer a predição
+
+    Args:
+        ticker: str -> Código do ticker a ser analisado
+    """
+    prediction = predict(ticker)
+    return {"ticker": str(ticker), "predicted": float(prediction)}
