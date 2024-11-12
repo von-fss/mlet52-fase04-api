a8b3acaeaa0b1cdd0098c512fd74cdd5dfe28e7a
von-fss <felipe.ruiz.s@gmail.com>
2024-11-12 20:16:27 -0300

    uncomment train.py

--- app/internal/train.py
+++ app/internal/train.py
@@ -4,8 +4,8 @@ from ..routers.utils import modelConfig
 
 def train_model(config: modelConfig) -> None:
     lstm_model = LSTM_model_train(config)
-    lstm_model.LSTMModel_train()
-    # lstm_model.save()
+    lstm_model.LSTMModel_train()    
+    lstm_model.save()
 
 ##### thinking about it
 # from .utils import modelConfig
