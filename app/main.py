aead034552fcf667ca691df3e19826ce720a7c53
Kassim <kassimorra@gmail.com>
2024-11-27 23:31:39 -0300

    adicionando o middleware para adicionar no retorno o tempo de processamento

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 from .routers import model, yfinance
 
 app = FastAPI()
@@ -6,6 +8,21 @@ app = FastAPI()
 app.include_router(model.router)
 app.include_router(yfinance.router)
 
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    print(response)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+
 @app.get('/')
 async def root():
     return {'message': 'Hello Bigget Applications'}
+
+
+
