2cb523d56ea7b975843df9764615cc8f290bacdc
Kassim <kassimorra@gmail.com>
2024-11-28 09:34:44 -0300

    atualização do middleware adicionando retorno no header do tempo de processamento, tempo de CPU e memória utilizada

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 import time
 
 from fastapi import FastAPI, Request
+
 from .routers import model, yfinance
+from pyinstrument import Profiler, renderers
+from memory_profiler import memory_usage
 
 app = FastAPI()
 
@@ -12,11 +15,21 @@ app.include_router(yfinance.router)
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
     start_time = time.perf_counter()
+    start_mem = memory_usage()[0]
+    profiler = Profiler(interval=0.001, async_mode="enabled")
+    profiler.start()
     response = await call_next(request)
-    print(response)
+    end_mem = memory_usage()[0]
     process_time = time.perf_counter() - start_time
+    profiler.stop()
+    stats = profiler.last_session
+
+    response.headers["X-CPU-Time"] = str(stats.cpu_time)
     response.headers["X-Process-Time"] = str(process_time)
+    response.headers["X-Memory-Usage"] = str(end_mem - start_mem)
+
     return response
+    #return await call_next(request)
 
 
 
